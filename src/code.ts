a21801083e53e3f421df2d4374791f802f83632b
Damian Wiśniewski <d.wisniewski@uniso.pl>
2019-02-16 11:36:11 +0100

    Add child observer.

---
+++ src/code.ts
@@ -0,0 +1,36 @@
+import { Observable } from 'rxjs/Observable';
+
+const observable = Observable.create((observer:any) => {
+  try {
+    observer.next('Hey guys!')
+    observer.next('How are you?')
+    setInterval(() => {
+      observer.next('I am good')
+    }, 2000)
+  } catch(err) {
+    observer.error(err)
+  }
+});
+
+const addItem = (val:any) => {
+  const node = document.createElement("li");
+  const textNode = document.createTextNode(val);
+  node.appendChild(textNode);
+  document.getElementById('output').appendChild(node);
+}
+
+const observer = observable.subscribe(
+  (x:any) => addItem(x),
+  (error:any) => addItem(error),
+  () => addItem('Completed')
+);
+
+const observer2 = observable.subscribe(
+  (x:any) => addItem(x),
+);
+
+observer.add(observer2)
+
+setTimeout(() => {
+  observer.unsubscribe();
+}, 6000)
